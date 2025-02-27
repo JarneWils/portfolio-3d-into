0314344f7f12807b6c312e1ca7513b1dbb1592e2
wilske.jarne@gmail.com <107676602+JarneWils@users.noreply.github.com>
2025-02-27 11:28:49 +0100

    final

--- vite.config.js
+++ vite.config.js
@@ -28,10 +28,10 @@ export default {
             },
         },
     ],
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    server: {
+        host: true,
+        allowedHosts: ['jarne-wils.onrender.com'], // Voeg je host hier toe
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
     },
     build:
     {
